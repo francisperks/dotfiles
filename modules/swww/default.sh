4e0a4509bbbf6d5c418d81449b120bff0ebedda2
Francis <55701146+francisperks@users.noreply.github.com>
2025-07-11 01:03:39 +0100

    test

--- modules/swww/default.sh
+++ modules/swww/default.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
-swww init &
+# Start the daemon if it's not already running
+pgrep swww-daemon >/dev/null || swww-daemon &
+
+# Give it a moment to initialize
 sleep 0.5
+
+# Set the wallpaper
 swww img "$HOME/Pictures/wallpapers/default.jpg" --transition-type any
